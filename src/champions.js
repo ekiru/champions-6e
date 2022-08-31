29d864131f4e8dc7eabc1f59e249b2f4ad114b41
Surinna Curtis <ekiru.0@gmail.com>
2022-08-31 18:32:27 -0500

    Move quench tests into tests/quench/

---
+++ src/champions.js
@@ -0,0 +1,5 @@
+
+Hooks.on("quenchReady", async function(quench) {
+    const { runTests } = await import('../tests/quench/init.js')
+    runTests(quench)
+})
---
+++ tests/quench/init.js
@@ -0,0 +1,17 @@
+const SYSTEM = "champions-6e"
+
+export function runTests(quench) {
+    quench.registerBatch(
+        `${SYSTEM}.quench.test-working`,
+        context => {
+            const { describe, it, expect } = context
+
+            describe("Quench test runner", function () {
+                it("should be able to run tests", function () {
+                    expect(2+2).to.equal(4)
+                })
+            })
+        },
+        { displayName: `${SYSTEM}: Test Quench is working` },
+    )
+}
