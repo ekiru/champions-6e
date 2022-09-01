4a545dc6b608b34061502b09502e858f9e254609
Surinna Curtis <ekiru.0@gmail.com>
2022-09-01 16:48:01 -0500

    Set up an empty ChampionsActor class.

--- src/champions.js
+++ src/champions.js
@@ -1,4 +1,11 @@
+import ChampionsActor from "./actor/ChampionsActor.js";
+
+// Register Quench tests if it's available.
 Hooks.on("quenchReady", async function (quench) {
   const { registerTests } = await import("../tests/quench/init.js");
   registerTests(quench);
+});
+
+Hooks.once("init", function () {
+  CONFIG.Actor.documentClass = ChampionsActor;
 });
