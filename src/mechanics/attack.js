2d97e24654962718e9f7b483c9b24a1f9a21ebdc
Surinna Curtis <ekiru.0@gmail.com>
2022-09-07 15:54:29 -0500

    Calculate highest DCV hit given an OCV and roll.

--- src/mechanics/attack.js
+++ src/mechanics/attack.js
@@ -1,4 +1,21 @@
 /**
+ * Calculates the highest DCV that an attacker can hit with a particular roll.
+ *
+ * @param {number} ocv The attacker's OCV.
+ * @param {number} roll The attacker's attack roll.
+ * @returns {number} The highest DCV that the attacker can hit with the roll.
+ */
+export function highestDcvHit(ocv, roll) {
+  if (roll === 3) {
+    return Number.POSITIVE_INFINITY;
+  }
+  if (roll === 18) {
+    return Number.NEGATIVE_INFINITY;
+  }
+  return ocv + 11 - roll;
+}
+
+/**
  * Calculates the target number for a roll to attack based on the attacker's OCV and
  * the target's DCV.
  *
--- tests/jest/rolls/attack-rolls.test.js
+++ tests/jest/rolls/attack-rolls.test.js
@@ -1,8 +1,11 @@
 /* eslint-env jest */
-import { targetNumberToHit } from "../../../src/mechanics/attack.js";
+import {
+  highestDcvHit,
+  targetNumberToHit,
+} from "../../../src/mechanics/attack.js";
 
 describe("Attack rolls", function () {
-  describe("targetNumberToHit()", function () {
+  describe("against a known DCV", function () {
     describe("with an OCV of 9", function () {
       const ocv = 9;
 
@@ -21,6 +24,30 @@ describe("Attack rolls", function () {
 
     it("always has a TN of at highest 17-", function () {
       expect(targetNumberToHit(99, 0)).toBe(17);
+    });
+  });
+
+  describe("against an unknown DCV", function () {
+    const ocv = 8;
+
+    it("should be able to hit DCV = OCV on a roll of 11", function () {
+      expect(highestDcvHit(ocv, 11)).toBe(ocv);
+    });
+
+    it("should be able to hit DCV = OCV + 1 on a roll of 10", function () {
+      expect(highestDcvHit(ocv, 10)).toBe(ocv + 1);
+    });
+
+    it("should be able to hit DCV = OCV - 2 on a roll of 13", function () {
+      expect(highestDcvHit(ocv, 13)).toBe(ocv - 2);
+    });
+
+    it("should be able to hit anything with a roll of 3", function () {
+      expect(highestDcvHit(ocv, 3)).toEqual(Infinity);
+    });
+
+    it("shouldn't be able to hit anything with a roll of 18", function () {
+      expect(highestDcvHit(ocv, 18)).toEqual(Number.NEGATIVE_INFINITY);
     });
   });
 });
