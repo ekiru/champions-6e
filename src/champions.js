c82ccd15eae6af2d8479296085ed96fad9e7d515
Surinna Curtis <ekiru.0@gmail.com>
2022-09-12 11:56:22 -0500

    Implement the Euclidean ruler.

--- src/champions.js
+++ src/champions.js
@@ -2,6 +2,7 @@ import ChampionsActor from "./actor/ChampionsActor.js";
 import ChampionsItem from "./item/ChampionsItem.js";
 import CharacterSheet from "./actor/CharacterSheet.js";
 import SkillSheet from "./item/SkillSheet.js";
+import EuclideanRuler from "./documents/EuclideanRuler.js";
 
 // Register Quench tests if it's available.
 Hooks.on("quenchReady", async function (quench) {
@@ -12,6 +13,7 @@ Hooks.on("quenchReady", async function (quench) {
 Hooks.once("init", function () {
   CONFIG.Actor.documentClass = ChampionsActor;
   CONFIG.Item.documentClass = ChampionsItem;
+  CONFIG.Canvas.rulerClass = EuclideanRuler;
 
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("champions-6e", CharacterSheet, {
