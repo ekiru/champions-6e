f28bb04b02e95436c66ce4f2a4fbe6806bdd39e1
Surinna Curtis <ekiru.0@gmail.com>
2022-09-14 13:52:28 -0500

    Update skill sheet.

--- src/item/SkillSheet.js
+++ src/item/SkillSheet.js
@@ -1,3 +1,5 @@
+import FieldBuilder from "../sheets/FieldBuilder.js";
+
 export default class SkillSheet extends ItemSheet {
   /** @override */
   static get defaultOptions() {
@@ -18,13 +20,43 @@ export default class SkillSheet extends ItemSheet {
   async getData(options = {}) {
     const context = super.getData(options);
 
+    const fields = new FieldBuilder(this.item);
+
     context.attributes = {
-      targetNumber: {
+      type: fields.selection("Type", "system.type", {
+        misc: "Miscellaneous",
+        characteristic: "Characteristic-based",
+      }),
+    };
+
+    if (context.attributes.type.value === "characteristic") {
+      context.attributes.bonus = fields.number("Bonus", "system.bonus.value");
+      context.attributes.characteristic = fields.selection(
+        "Characteristic",
+        "system.characteristic",
+        {
+          dex: "DEX",
+          int: "INT",
+          pre: "PRE",
+        }
+      );
+      context.attributes.level = fields.selection("Level", "system.level", {
+        familiarity: "Familiarity (8-)",
+        proficiency: "Proficiency (10-)",
+        full: "Full",
+      });
+      context.attributes.targetNumber = {
+        label: "Target Number",
+        value: this.item.targetNumber,
+        readonly: true,
+      };
+    } else if (context.attributes.type.value === "misc") {
+      context.attributes.targetNumber = {
         label: "Target Number (0 for N/A)",
         value: this.item.system.targetNumber.value,
         path: "system.targetNumber.value",
-      },
-    };
+      };
+    }
 
     context.bio = {
       description: {
