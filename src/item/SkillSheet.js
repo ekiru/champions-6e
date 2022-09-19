2ef84f63b0030583d5903d813257ed96916c03ef
Surinna Curtis <ekiru.0@gmail.com>
2022-09-19 13:49:02 -0500

    Skill sheet support for skill levels

--- src/item/SkillSheet.js
+++ src/item/SkillSheet.js
@@ -1,4 +1,5 @@
 import FieldBuilder from "../sheets/FieldBuilder.js";
+import * as assert from "../util/assert.js";
 
 const SKILL_CHARACTERISTICS = {
   dex: "DEX",
@@ -33,6 +34,7 @@ export default class SkillSheet extends ItemSheet {
         misc: "Miscellaneous",
         background: "Background",
         characteristic: "Characteristic-based",
+        skillLevel: "Skill Level",
       }),
     };
 
@@ -79,6 +81,21 @@ export default class SkillSheet extends ItemSheet {
         "targetNumber",
         { readonly: true }
       );
+    } else if (context.attributes.type.value === "skillLevel") {
+      context.attributes.skillLevel = {
+        amount: fields.number("Number of Levels", "system.skillLevel.amount"),
+        class: fields.selection("Class", "system.skillLevel.class", {
+          skill: "Skill Level",
+          combat: "Combat Skill Level",
+          ocvPenalty: "Penalty Skill Level (OCV)",
+          dcvPenalty: "Penalty Skill Level (DCV)",
+        }),
+      };
+      context.attributes.skillLevel.type = fields.selection(
+        "Type",
+        "system.skillLevel.type",
+        this.#skillLevelTypes(context.attributes.skillLevel.class.value)
+      );
     } else if (context.attributes.type.value === "misc") {
       context.attributes.targetNumber = fields.number(
         "Target Number (0 for N/A)",
@@ -91,5 +108,45 @@ export default class SkillSheet extends ItemSheet {
     };
 
     return context;
+  }
+
+  #skillLevelTypes(cls) {
+    switch (cls) {
+      case "skill":
+        return {
+          singleSkill: "Single Skill or Characteristic",
+          threeSkills: "Three Skills",
+          broadGroup:
+            "Broad Group (e.g. all Intellect skills, all Interaction skills, all medical skills)",
+          allDex: "All Agility skills",
+          allNoncombat: "All Noncombat",
+          overall: "Overall",
+        };
+      case "combat":
+        return {
+          singleAttack:
+            "Specific (a single Combat Maneuver, Martial Maneuver, weapon, or power)",
+          smallGroup:
+            "Small Group (up to three related maneuvers, a tight group of weapons, up to three related powers)",
+          largeGroup:
+            "Large Group (more than a small group, less than all HTH/all ranged)",
+          allHthRanged: "All HTH/All Ranged",
+          allCombat: "All Combat",
+        };
+      case "ocvPenalty":
+        return {
+          singleAttack: "A specific penalty for a single attack",
+          tightGroup: "A specific penalty for a tight group of attacks",
+          allAttacks: "A specific penalty for all attacks",
+        };
+      case "dcvPenalty":
+        return {
+          singleCondition: "A specific penalty imposed by a single condition",
+          groupOfConditions:
+            "A specific penalty imposed by a group of conditions",
+        };
+      default:
+        assert.precondition(false, `Invalid Skill Level class: ${cls}`);
+    }
   }
 }
