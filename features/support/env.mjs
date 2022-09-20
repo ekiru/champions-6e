388f617bc77a91e9019fa709b5776e81634cfae9
Surinna Curtis <ekiru.0@gmail.com>
2022-09-20 14:18:08 -0500

    Cucumber tests try log in to foundry
    I thought this was successful, but it was not. Tabling it for now.

--- features/support/env.mjs
+++ features/support/env.mjs
@@ -1,4 +1,5 @@
 import { spawn } from "node:child_process";
+import { setTimeout } from "node:timers/promises";
 import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
 import { firefox } from "playwright";
 
@@ -10,16 +11,28 @@ BeforeAll(async function () {
     [`${process.env.HOME}/src/foundryvtt/resources/app/main.js`],
     { stdio: "ignore" }
   );
+  await setTimeout(200);
   browser = await firefox.launch();
 });
 
+BeforeAll(async function () {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:30000/");
+  await page.click(
+    'li[data-package-id="zz-champions-automated-testing"] >> button[name="action"]'
+  );
+});
+
 AfterAll(async function () {
-  await browser.close();
   foundryProcess.kill("SIGKILL");
+  await browser.close();
 });
 
 Before(async function () {
   this.page = await browser.newPage();
+  await this.page.goto("http://localhost:30000/");
+  await this.page.selectOption("[name=userid]", { label: "Gamemaster" });
+  await this.page.click("text=Join Game Session");
 });
 
 After(async function () {
