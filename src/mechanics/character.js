6d5911679bb1d3a4245a4fd15cd1661b550e80ca
Surinna Curtis <ekiru.0@gmail.com>
2022-10-24 12:57:59 -0500

    begin exposing characteristics.

--- src/mechanics/character.js
+++ src/mechanics/character.js
@@ -1,7 +1,65 @@
+import * as assert from "../util/assert.js";
+import {
+  Characteristic,
+  byName as characteristicByName,
+} from "./characteristics.js";
+
+/**
+ * @typedef CharacteristicValue
+ * @property {number} value The base or current value of the characteristic. For
+ * modifiable characteristics, it's the base value. For resources, it's the current
+ * value.
+ * @property {number?} modifier A modifier applied to the characteristic's base value.
+ */
+
+/**
+ * Represents a HERO System 6E Character.
+ *
+ * @param {string} name The character's name.
+ */
 export class Character {
+  /**
+   * The name of the character.
+   *
+   * Many Champions characters have multiple identities. Which is chosen as the name
+   * and which is reserved for alter egos is up to the player.
+   *
+   * @type {string}
+   */
   name;
 
-  constructor(name) {
+  #characteristics = new Map();
+
+  constructor(name, { characteristics = {} } = {}) {
     this.name = name;
+    for (const [charName, data] of Object.entries(characteristics)) {
+      const char = characteristicByName(charName);
+      assert.precondition(
+        char !== undefined,
+        `No such characteristic: ${charName}`
+      );
+      this.#characteristics.set(char, data);
+    }
+  }
+
+  /**
+   * Retrieves the character's value for the characteristic.
+   *
+   * @param {Characteristic} char The characteristic.
+   * @returns {CharacteristicValue} The value and other information for the characteristic.
+   */
+  characteristic(char) {
+    return this.#characteristics.get(char);
+  }
+
+  /**
+   * Updates a characteristic with new values.
+   *
+   * @param {Characteristic} charName The characteristic to update.
+   * @param {*} changes Changes to apply to the characteristic values.
+   */
+  setCharacteristic(charName, changes) {
+    const char = this.characteristic(charName);
+    Object.assign(char, changes);
   }
 }
--- tests/jest/characters.test.js
+++ tests/jest/characters.test.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 
 import { Character } from "../../src/mechanics/character.js";
+import { STR } from "../../src/mechanics/characteristics.js";
 
 describe("Characters", function () {
   describe("constructor", function () {
@@ -13,6 +14,29 @@ describe("Characters", function () {
     it("should set the character's name", function () {
       const character = new Character("Vithraxian");
       expect(character.name).toBe("Vithraxian");
+    });
+  });
+
+  describe("characteristics", function () {
+    it("characteristic(STR) should expose the character's STR", function () {
+      const character = new Character("Ruby Ray", {
+        characteristics: { STR: { value: 10, modifier: +30 } },
+      });
+      expect(character.characteristic(STR)).toEqual({
+        value: 10,
+        modifier: +30,
+      });
+    });
+
+    it("setCharacteristic() should update supplied properties of the characteristic", function () {
+      const character = new Character("Mariyat", {
+        characteristics: { STR: { value: 20, modifier: +0 } },
+      });
+      character.setCharacteristic(STR, { modifier: +10 });
+      expect(character.characteristic(STR)).toEqual({
+        value: 20,
+        modifier: +10,
+      });
     });
   });
 });
