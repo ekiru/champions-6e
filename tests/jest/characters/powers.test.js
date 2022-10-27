7d2a0dff10df199cf7176a2563c1c8ed179e0428
Surinna Curtis <ekiru.0@gmail.com>
2022-10-27 12:28:19 -0500

    move powertype subclass tests into describe(PT)

--- tests/jest/characters/powers.test.js
+++ tests/jest/characters/powers.test.js
@@ -59,128 +59,128 @@ describe("PowerType", function () {
       expect(() => new PowerType().name).toThrow(assert.AbstractMethodError);
     });
   });
-});
 
-describe("StandardPowerType", function () {
-  it("extends PowerType", function () {
-    expect(StandardPowerType.get("Aid")).toBeInstanceOf(PowerType);
-  });
-
-  it("implements all abstract methods", function () {
-    expect(StandardPowerType.get("Blast")).toImplementPowerType();
-  });
-
-  describe("Powers", function () {
-    it("is an Enum", function () {
-      expect(StandardPowerType.Powers).toBeInstanceOf(Enum);
+  describe("StandardPowerType", function () {
+    it("extends PowerType", function () {
+      expect(StandardPowerType.get("Aid")).toBeInstanceOf(PowerType);
     });
 
-    it("has every power from CC 6E", function () {
-      const powers = [
-        "Absorption",
-        "Aid",
-        "Barrier",
-        "Blast",
-        "Cannot Be Stunned",
-        "Change Environment",
-        "Characteristics",
-        "Clairsentience",
-        "Clinging",
-        "Damage Negation",
-        "Damage Reduction",
-        "Darkness",
-        "Deflection",
-        "Density Increase",
-        "Desolidification",
-        "Dispel",
-        "Does not Bleed",
-        "Drain",
-        "Duplication",
-        "Endurance Reserve",
-        "Enhanced Senses",
-        "Entangle",
-        "Extra-Dimensional Movement",
-        "Extra Limbs",
-        "FTL Travel",
-        "Flash",
-        "Flash Defense",
-        "Flight",
-        "Growth",
-        "Hand-To-Hand Attack",
-        "Healing",
-        "Images",
-        "Invisibility",
-        "Killing Attack",
-        "Knockback Resistance",
-        "Leaping",
-        "Life Support",
-        "Luck",
-        "Mental Blast",
-        "Mental Illusions",
-        "Mind Control",
-        "Mind Link",
-        "Mind Scan",
-        "Multiform",
-        "No Hit Locations",
-        "Power Defense",
-        "Reflection",
-        "Regeneration",
-        "Resistant Protection",
-        "Running",
-        "Shape Shift",
-        "Shrinking",
-        "Skills",
-        "Stretching",
-        "Summon",
-        "Swimming",
-        "Swinging",
-        "Takes No STUN",
-        "Telekinesis",
-        "Transform",
-        "Tunneling",
-      ];
-      const found = [];
-      for (const power of powers) {
-        if (StandardPowerType.Powers[power]) {
-          found.push(power);
+    it("implements all abstract methods", function () {
+      expect(StandardPowerType.get("Blast")).toImplementPowerType();
+    });
+
+    describe("Powers", function () {
+      it("is an Enum", function () {
+        expect(StandardPowerType.Powers).toBeInstanceOf(Enum);
+      });
+
+      it("has every power from CC 6E", function () {
+        const powers = [
+          "Absorption",
+          "Aid",
+          "Barrier",
+          "Blast",
+          "Cannot Be Stunned",
+          "Change Environment",
+          "Characteristics",
+          "Clairsentience",
+          "Clinging",
+          "Damage Negation",
+          "Damage Reduction",
+          "Darkness",
+          "Deflection",
+          "Density Increase",
+          "Desolidification",
+          "Dispel",
+          "Does not Bleed",
+          "Drain",
+          "Duplication",
+          "Endurance Reserve",
+          "Enhanced Senses",
+          "Entangle",
+          "Extra-Dimensional Movement",
+          "Extra Limbs",
+          "FTL Travel",
+          "Flash",
+          "Flash Defense",
+          "Flight",
+          "Growth",
+          "Hand-To-Hand Attack",
+          "Healing",
+          "Images",
+          "Invisibility",
+          "Killing Attack",
+          "Knockback Resistance",
+          "Leaping",
+          "Life Support",
+          "Luck",
+          "Mental Blast",
+          "Mental Illusions",
+          "Mind Control",
+          "Mind Link",
+          "Mind Scan",
+          "Multiform",
+          "No Hit Locations",
+          "Power Defense",
+          "Reflection",
+          "Regeneration",
+          "Resistant Protection",
+          "Running",
+          "Shape Shift",
+          "Shrinking",
+          "Skills",
+          "Stretching",
+          "Summon",
+          "Swimming",
+          "Swinging",
+          "Takes No STUN",
+          "Telekinesis",
+          "Transform",
+          "Tunneling",
+        ];
+        const found = [];
+        for (const power of powers) {
+          if (StandardPowerType.Powers[power]) {
+            found.push(power);
+          }
         }
-      }
-      expect(found).toStrictEqual(powers);
+        expect(found).toStrictEqual(powers);
+      });
+    });
+
+    describe("get", function () {
+      it("returns a power with the right name", function () {
+        expect(StandardPowerType.get("Shape Shift").name).toBe("Shape Shift");
+      });
+
+      it("returns the same value each time", function () {
+        expect(
+          Object.is(
+            StandardPowerType.get("Drain"),
+            StandardPowerType.get("Drain")
+          )
+        ).toBe(true);
+      });
+
+      it("throws for non-existent powers", function () {
+        expect(() => StandardPowerType.get("Bookworm")).toThrow(
+          "There is no standard power named"
+        );
+      });
     });
   });
 
-  describe("get", function () {
-    it("returns a power with the right name", function () {
-      expect(StandardPowerType.get("Shape Shift").name).toBe("Shape Shift");
+  describe("CustomPowerType", function () {
+    it("extends PowerType", function () {
+      expect(new CustomPowerType("Poison")).toBeInstanceOf(PowerType);
     });
 
-    it("returns the same value each time", function () {
-      expect(
-        Object.is(
-          StandardPowerType.get("Drain"),
-          StandardPowerType.get("Drain")
-        )
-      ).toBe(true);
+    it("implements all abstract methods", function () {
+      expect(new CustomPowerType("Poison")).toImplementPowerType();
     });
 
-    it("throws for non-existent powers", function () {
-      expect(() => StandardPowerType.get("Bookworm")).toThrow(
-        "There is no standard power named"
-      );
+    it("round trips name", function () {
+      expect(new CustomPowerType("Poison").name).toBe("Poison");
     });
-  });
-});
-
-describe("CustomPowerType", function () {
-  it("extends PowerType", function () {
-    expect(new CustomPowerType("Poison")).toBeInstanceOf(PowerType);
-  });
-
-  it("implements all abstract methods", function () {
-    expect(new CustomPowerType("Poison")).toImplementPowerType();
-  });
-
-  it("round trips name", function () {
-    expect(new CustomPowerType("Poison").name).toBe("Poison");
   });
 });
